75033d9ce0e423482e79ecdb2ebb2f4f61574ad9
Miranda Bradley <mirandajbradley@gmail.com>
2015-02-01 15:58:28 -0500

    char moves on own!

--- GameJamTreasureChest/Assets/HeroHandler.cs
+++ GameJamTreasureChest/Assets/HeroHandler.cs
@@ -5,12 +5,17 @@ using System.Collections.Generic;
 public class HeroHandler : MonoBehaviour {
 	StoppableCoroutine move, move2;
 	List<float> distances = new List<float>();
+	Dictionary<dir, Vector2> dirVector = new Dictionary<dir, Vector2>();
 
 	// Use this for initialization
 	void Start () {
 		Debug.Log("hero");
 		move = new StoppableCoroutine(movePattern());
 		StartCoroutine(move);
+		dirVector.Add (dir.left, new Vector2(-1,0));
+		dirVector.Add (dir.right, new Vector2(1,0));
+		dirVector.Add (dir.up, new Vector2(0,1));
+		dirVector.Add (dir.down, new Vector2(0,-1));
 	}
 	
 	// Update is called once per frame
@@ -20,7 +25,7 @@ public class HeroHandler : MonoBehaviour {
 	public List<dir> FindClearDirections(){
 		List<dir> movable = new List<dir>();
 		distances = new List<float>();
-		if (CheckDir(new Vector2(-1,0))) movable.Add(dir.left);
+		if(CheckDir(new Vector2(-1,0))) movable.Add(dir.left);
 		if(CheckDir(new Vector2(1,0))) movable.Add(dir.right);
 		if(CheckDir(new Vector2(0,1))) movable.Add(dir.up);
 		if(CheckDir(new Vector2(0,-1))) movable.Add(dir.down);
@@ -41,9 +46,23 @@ public class HeroHandler : MonoBehaviour {
 		}
 	}
 	
-	public void MoveHero(dir d, StoppableCoroutine s){
-		s = new StoppableCoroutine(MovementHandler.instance.MoveDir(d, this.transform));
-		StartCoroutine(s);
+	public void MoveHero(dir d){
+		move2 = new StoppableCoroutine(MovementHandler.instance.MoveDir(d, this.transform));
+		StartCoroutine(move2);
+		RecheckDist(dirVector[d], d);
+	}
+	
+	public void RecheckDist(Vector2 v, dir d){
+		Debug.Log("to check: " + v);
+		RaycastHit2D hit = Physics2D.Raycast(this.transform.position, v);
+		if(hit.collider != null){
+			float distance = Vector2.Distance(hit.point, this.transform.position);
+			if(distance > 2f) MoveHero(d);
+			else {
+				move = new StoppableCoroutine(movePattern());
+				StartCoroutine(move);
+			}
+		}
 	}
 	
 	public dir DecideDirection(List<dir> d){
@@ -62,6 +81,6 @@ public class HeroHandler : MonoBehaviour {
 		yield return new WaitForSeconds(0.01f);
 		dir d = DecideDirection(FindClearDirections());
 		Debug.Log("should move " + d);
-		MoveHero(d, move2);
+		MoveHero(d);
 	}
 }
